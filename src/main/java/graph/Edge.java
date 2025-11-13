eed5d49a4e9be69b5c85fc7befa9ac21bb2be130
Lamgy <kloverpoopa@gmail.com>
2025-11-13 21:55:33 +0500

    feat/graph - add graph and edge classes

---
+++ src/main/java/graph/Edge.java
@@ -0,0 +1,27 @@
+package graph;
+
+public class Edge implements Comparable<Edge> {
+    private String from;
+    private String to;
+    private double weight;
+
+    public Edge(String from, String to, double weight) {
+        this.from = from;
+        this.to = to;
+        this.weight = weight;
+    }
+
+    public String getFrom() { return from; }
+    public String getTo() { return to; }
+    public double getWeight() { return weight; }
+
+    @Override
+    public int compareTo(Edge other) {
+        return Double.compare(this.weight, other.weight);
+    }
+
+    @Override
+    public String toString() {
+        return from + " - " + to + " (" + weight + ")";
+    }
+}
